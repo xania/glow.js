319655c2cd2b96caee73ee4ba78d146f56f42129
xania <ibrahim.bensalah@gmail.com>
2020-10-14 16:43:55 +0200

    prettier

--- lib/fragment.ts
+++ lib/fragment.ts
@@ -1,9 +1,12 @@
 import { ITemplate } from './driver.js';
 
 export function Fragment(_props: {}, children?: any[]): any[] | undefined {
-  return children;
+    return children;
 }
 
-export default function(_props: {}, children?: any[]): ITemplate[] | undefined {
-  return children;
+export default function (
+    _props: {},
+    children?: any[]
+): ITemplate[] | undefined {
+    return children;
 }
