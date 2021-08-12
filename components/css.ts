602af76e84fcaad36d95e03ab92cb1f183224f03
Ibrahim <34893682+xania@users.noreply.github.com>
2021-08-12 21:39:19 +0200

    white spaces

--- components/css.ts
+++ components/css.ts
@@ -2,51 +2,51 @@ import { IDriver } from '../lib/driver';
 import { Subscribable } from 'rxjs';
 
 interface CssProps {
-    value: string;
-    when?: Subscribable<boolean>;
+  value: string;
+  when?: Subscribable<boolean>;
 }
 
 export default function Css(props: CssProps) {
-    return {
-        render(driver: IDriver) {
-            const { when, value } = props;
-            if (when && typeof when.subscribe === 'function') {
-                const binding = driver.createAttribute('class', undefined);
+  return {
+    render(driver: IDriver) {
+      const { when, value } = props;
+      if (when && typeof when.subscribe === 'function') {
+        const binding = driver.createAttribute('class', undefined);
 
-                when.subscribe((e) => {
-                    if (e) binding.next(value);
-                    else binding.next([]);
-                });
+        when.subscribe((e) => {
+          if (e) binding.next(value);
+          else binding.next([]);
+        });
 
-                return binding;
-            } else {
-                return driver.createAttribute('class', value);
-            }
-        },
-    };
+        return binding;
+      } else {
+        return driver.createAttribute('class', value);
+      }
+    },
+  };
 }
 
 interface AttrProps {
-    value: any;
-    name: string;
-    when?: Subscribable<boolean>;
+  value: any;
+  name: string;
+  when?: Subscribable<boolean>;
 }
 export function Attr(props: AttrProps) {
-    return {
-        render(driver: IDriver) {
-            const { when, name, value } = props;
-            if (when && typeof when.subscribe === 'function') {
-                const binding = driver.createAttribute(name, undefined);
+  return {
+    render(driver: IDriver) {
+      const { when, name, value } = props;
+      if (when && typeof when.subscribe === 'function') {
+        const binding = driver.createAttribute(name, undefined);
 
-                when.subscribe((e) => {
-                    if (e) binding.next(value);
-                    else binding.next([]);
-                });
+        when.subscribe((e) => {
+          if (e) binding.next(value);
+          else binding.next([]);
+        });
 
-                return binding;
-            } else {
-                return driver.createAttribute(name, value);
-            }
-        },
-    };
+        return binding;
+      } else {
+        return driver.createAttribute(name, value);
+      }
+    },
+  };
 }
